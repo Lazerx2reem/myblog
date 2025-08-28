f4d7280ba78e7fe7528bfd2524e54a282ce51dfe
Arsh Gupta <arsh121523@gmail.com>
2025-08-28 11:55:56 -0400

    Updated posts.js

---
+++ src/data/posts.js
@@ -0,0 +1,14 @@
+const posts = [
+  {
+    slug: "post1",
+    title: "My First Blog Post",
+    date: "August 28, 2025",
+  },
+  {
+    slug: "post2",
+    title: "Learning React",
+    date: "September 1, 2025",
+  },
+];
+
+export default posts;
